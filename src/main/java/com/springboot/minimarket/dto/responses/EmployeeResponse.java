8200973ca43343b59a1a41297406e8adc37de92a
Tharissa Najwa <tharissaazz@gmail.com>
2023-09-19 21:26:42 +0700

    remove unused getter setter

--- src/main/java/com/springboot/minimarket/dto/responses/EmployeeResponse.java
+++ src/main/java/com/springboot/minimarket/dto/responses/EmployeeResponse.java
@@ -23,36 +23,4 @@ public class EmployeeResponse {
         this.address = employee.getAddress();
         this.phoneNumber = employee.getPhoneNumber();
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
--- src/main/java/com/springboot/minimarket/dto/responses/MemberResponse.java
+++ src/main/java/com/springboot/minimarket/dto/responses/MemberResponse.java
@@ -27,44 +27,4 @@ public class MemberResponse {
         this.phoneNumber = member.getPhoneNumber();
         this.point = member.getPoint();
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Integer getPoint() {
-        return point;
-    }
-
-    public void setPoint(Integer point) {
-        this.point = point;
-    }
 }
