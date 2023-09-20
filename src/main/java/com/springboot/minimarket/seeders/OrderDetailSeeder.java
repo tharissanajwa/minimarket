d0a8e2dfac350a64b7d6f48a0400309f4080fc47
Tharissa Najwa <tharissaazz@gmail.com>
2023-09-21 00:19:08 +0700

    added more seeder for order, order detail and payment

--- src/main/java/com/springboot/minimarket/seeders/OrderDetailSeeder.java
+++ src/main/java/com/springboot/minimarket/seeders/OrderDetailSeeder.java
@@ -60,6 +60,15 @@ public class OrderDetailSeeder {
                 new OrderDetail(orderService.getOrderById(4L), productService.getProductById(4L), 1, 7000, 0, 7000),
                 new OrderDetail(orderService.getOrderById(4L), productService.getProductById(13L), 2, 6500, 0, 13000),
 
+                // Order detail untuk id order 5
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(4L), 2, 7000, 0, 14000),
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(9L), 2, 6500, 500, 12000),
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(12L), 1, 3000, 0, 3000),
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(7L), 1, 55000, 0, 55000),
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(3L), 4, 5500, 0, 22000),
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(1L), 1, 5000, 0, 5000),
+                new OrderDetail(orderService.getOrderById(5L), productService.getProductById(8L), 2, 9000, 500, 17000),
+
                 // Order detail untuk id order 6
                 new OrderDetail(orderService.getOrderById(6L), productService.getProductById(1L), 2, 5000, 0, 10000),
                 new OrderDetail(orderService.getOrderById(6L), productService.getProductById(2L), 3, 4500, 500, 12000),
@@ -68,6 +77,46 @@ public class OrderDetailSeeder {
                 new OrderDetail(orderService.getOrderById(6L), productService.getProductById(6L), 2, 15000, 0, 30000),
                 new OrderDetail(orderService.getOrderById(6L), productService.getProductById(5L), 1, 5000, 0, 5000),
                 new OrderDetail(orderService.getOrderById(6L), productService.getProductById(14L), 1, 11000, 0, 11000),
+
+                // Order detail untuk id order 7
+                new OrderDetail(orderService.getOrderById(7L), productService.getProductById(4L), 2, 7000, 0, 14000),
+                new OrderDetail(orderService.getOrderById(7L), productService.getProductById(9L), 2, 6500, 500, 12000),
+                new OrderDetail(orderService.getOrderById(7L), productService.getProductById(12L), 1, 3000, 0, 3000),
+                new OrderDetail(orderService.getOrderById(7L), productService.getProductById(13L), 4, 6500, 500, 24000),
+                new OrderDetail(orderService.getOrderById(7L), productService.getProductById(2L), 1, 4500, 0, 4500),
+                new OrderDetail(orderService.getOrderById(7L), productService.getProductById(14L), 2, 11000, 0, 22000),
+
+                // Order detail untuk id order 8
+                new OrderDetail(orderService.getOrderById(8L), productService.getProductById(4L), 2, 7000, 0, 14000),
+                new OrderDetail(orderService.getOrderById(8L), productService.getProductById(9L), 2, 6500, 500, 12000),
+                new OrderDetail(orderService.getOrderById(8L), productService.getProductById(12L), 1, 3000, 0, 3000),
+                new OrderDetail(orderService.getOrderById(8L), productService.getProductById(6L), 4, 15000, 0, 60000),
+                new OrderDetail(orderService.getOrderById(8L), productService.getProductById(3L), 1, 5500, 0, 5500),
+                new OrderDetail(orderService.getOrderById(8L), productService.getProductById(5L), 2, 5000, 0, 10000),
+
+                // Order detail untuk id order 9
+                new OrderDetail(orderService.getOrderById(9L), productService.getProductById(7L), 1, 55000, 0, 14000),
+                new OrderDetail(orderService.getOrderById(9L), productService.getProductById(11L), 2, 3200, 200, 6000),
+                new OrderDetail(orderService.getOrderById(9L), productService.getProductById(15L), 4, 2500, 0, 10000),
+                new OrderDetail(orderService.getOrderById(9L), productService.getProductById(4L), 3, 7000, 0, 21000),
+                new OrderDetail(orderService.getOrderById(9L), productService.getProductById(1L), 1, 5000, 0, 5000),
+                new OrderDetail(orderService.getOrderById(9L), productService.getProductById(6L), 2, 15000, 0, 30000),
+
+                // Order detail untuk id order 10
+                new OrderDetail(orderService.getOrderById(10L), productService.getProductById(7L), 1, 55000, 0, 14000),
+                new OrderDetail(orderService.getOrderById(10L), productService.getProductById(11L), 2, 3200, 200, 6000),
+                new OrderDetail(orderService.getOrderById(10L), productService.getProductById(15L), 4, 2500, 0, 10000),
+                new OrderDetail(orderService.getOrderById(10L), productService.getProductById(6L), 3, 15000, 0, 45000),
+                new OrderDetail(orderService.getOrderById(10L), productService.getProductById(12L), 1, 3000, 0, 3000),
+                new OrderDetail(orderService.getOrderById(10L), productService.getProductById(3L), 2, 5500, 1000, 9000),
+
+                // Order detail untuk id order 11
+                new OrderDetail(orderService.getOrderById(11L), productService.getProductById(7L), 1, 55000, 0, 14000),
+                new OrderDetail(orderService.getOrderById(11L), productService.getProductById(11L), 2, 3200, 200, 6000),
+                new OrderDetail(orderService.getOrderById(11L), productService.getProductById(15L), 4, 2500, 0, 10000),
+                new OrderDetail(orderService.getOrderById(11L), productService.getProductById(4L), 3, 7000, 0, 21000),
+                new OrderDetail(orderService.getOrderById(11L), productService.getProductById(1L), 1, 5000, 0, 5000),
+                new OrderDetail(orderService.getOrderById(11L), productService.getProductById(8L), 2, 9000, 1000, 16000),
 
                 // Order detail untuk id order 12
                 new OrderDetail(orderService.getOrderById(12L), productService.getProductById(1L), 2, 5000, 0, 10000),
--- src/main/java/com/springboot/minimarket/seeders/OrderSeeder.java
+++ src/main/java/com/springboot/minimarket/seeders/OrderSeeder.java
@@ -28,15 +28,15 @@ public class OrderSeeder {
         List<Order> orders = new ArrayList<>(Arrays.asList(
                 new Order("ORD-1", employeeService.getEmployeeById(1L), memberService.getMemberById(1L), 8, 106000, 10600, true, new Date(1694563200000L)), // Tanggal 2023-09-13
                 new Order("ORD-2", employeeService.getEmployeeById(2L), memberService.getMemberById(2L), 6, 54500, 5450, true, new Date(1694649600000L)), // Tanggal 2023-09-14
-                new Order("ORD-3", employeeService.getEmployeeById(2L), memberService.getMemberById(3L), 6, 61000, 6100, false, new Date(1694736000000L)), // Tanggal 2023-09-15
+                new Order("ORD-3", employeeService.getEmployeeById(2L), memberService.getMemberById(3L), 6, 61000, 6100, true, new Date(1694736000000L)), // Tanggal 2023-09-15
                 new Order("ORD-4", employeeService.getEmployeeById(4L), memberService.getMemberById(2L), 7, 226000, 22600, true, new Date(1694563200000L)), // Tanggal 2023-09-13
-                new Order("ORD-5", employeeService.getEmployeeById(5L), memberService.getMemberById(4L), 0, 0, 0, false, new Date(1694822400000L)), // Tanggal 2023-09-16
+                new Order("ORD-5", employeeService.getEmployeeById(5L), memberService.getMemberById(4L), 7, 128000, 12800, false, new Date(1694822400000L)), // Tanggal 2023-09-16
                 new Order("ORD-6", employeeService.getEmployeeById(5L), null, 7, 106000, 0, false, new Date(1694649600000L)), // Tanggal 2023-09-14
-                new Order("ORD-7", employeeService.getEmployeeById(5L), memberService.getMemberById(5L), 0, 0, 0, false, new Date(1694736000000L)), // Tanggal 2023-09-15
-                new Order("ORD-8", employeeService.getEmployeeById(6L), memberService.getMemberById(3L), 0, 0, 0, false, new Date(1694563200000L)), // Tanggal 2023-09-13
-                new Order("ORD-9", employeeService.getEmployeeById(8L), memberService.getMemberById(3L), 0, 0, 0, false, new Date(1694908800000L)), // Tanggal 2023-09-17
-                new Order("ORD-10", employeeService.getEmployeeById(9L), memberService.getMemberById(9L), 0, 0, 0, false, new Date(1694995200000L)), // Tanggal 2023-09-18
-                new Order("ORD-11", employeeService.getEmployeeById(10L), memberService.getMemberById(1L), 0, 0, 0, false, new Date(1695056400000L)), // Tanggal 2023-09-19
+                new Order("ORD-7", employeeService.getEmployeeById(5L), memberService.getMemberById(5L), 6, 79500, 7950, true, new Date(1694736000000L)), // Tanggal 2023-09-15
+                new Order("ORD-8", employeeService.getEmployeeById(6L), memberService.getMemberById(3L), 6, 104500, 10450, true, new Date(1694563200000L)), // Tanggal 2023-09-13
+                new Order("ORD-9", employeeService.getEmployeeById(8L), memberService.getMemberById(3L), 6, 86000, 8600, true, new Date(1694908800000L)), // Tanggal 2023-09-17
+                new Order("ORD-10", employeeService.getEmployeeById(9L), memberService.getMemberById(9L), 6, 87000, 8700, true, new Date(1694995200000L)), // Tanggal 2023-09-18
+                new Order("ORD-11", employeeService.getEmployeeById(10L), memberService.getMemberById(1L), 6, 72000, 7200, true, new Date(1695056400000L)), // Tanggal 2023-09-19
                 new Order("ORD-12", employeeService.getEmployeeById(7L), null, 6, 70000, 0, true, new Date(1695056400000L)) // Tanggal 2023-09-19
         ));
 
